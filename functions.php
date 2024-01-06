b3e6fdab4dc9900861c78beb42c94df6571becc3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-06 01:20:28 +0100

    WP customization 1/2

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function polypress_theme_support(){
+    // Adds dynamic title tag support
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+}
+
+add_action('after_setup_theme', 'polypress_theme_support');
+
+function polypress_menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Right Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'polypress_menus');
+
+function polypress_register_styles(){
+
+    $version = wp_get_theme()->get('Version');
+
+    wp_enqueue_style('polypress-style', get_template_directory_uri() . '/styles.css', array('polypress-polykit'), $version, 'all');
+
+    wp_enqueue_style('polypress-polykit',"https://polykit.xyz/app/css/polykit-v0-1.css", array(), '0.1', 'all');
+    
+}
+
+add_action('wp_enqueue_scripts', 'polypress_register_styles');
+
+function polypress_register_scripts(){
+
+    wp_enqueue_script('polypress-polykit', "https://polykit.xyz/app/js/polykit-v0-1.js", array(), '0.0', true);
+
+    wp_enqueue_script('polypress-main', get_template_directory_uri() . '/assets/js/main.js', array(), '0.1', true);
+    
+}
+
+add_action('wp_enqueue_scripts', 'polypress_register_scripts');
+
+?>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    
+    <?php wp_head(); ?>
+
+</head>
+<body class="dark-mode">
+    <header>
+
+        <?php
+        
+        if(function_exists('the_custom_logo')){
+
+            $custom_logo_id = get_theme_mod('custom_logo');
+            $logo = wp_get_attachment_image_src($custom_logo_id);
+
+        }
+        
+        ?>
+
+        <img src="<?php echo $logo[0]; ?>" alt="">
+        <a href="<?php get_bloginfo  ?>"></a>
+
+        <nav>
+
+            <?php
+                wp_nav_menu(
+                    array(
+                        'menu' => 'primary',
+                        'container' => '',
+                        'theme_location' => 'primary',
+                        'items_wrap' => '<ul id="" class="">%3$s</ul>'
+                    )
+                );
+            ?>
+
+        </nav>
+    </header>
