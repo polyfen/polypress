6d7eef7959a30587e2e4d4aeaaf1655577bee66b
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 20:21:25 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -2,18 +2,13 @@
     get_header();
 ?>
 
-<article class="container">
+<section class="cover">
+    <h1 class="title-1 align-center">Upscale your Business<br>with our Proben Formula</h1>
+    <p class="align-center">We've helped Y Combinator start-ups<br>launch with amazing success.</p>
+    <a class="button">Learn More</a>
+</section>
 
-<?php
-    if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post();
-            the_content();
-        }
-    }
-?>
-
-</article>
+<hr class="secondary-separator">
 
 <?php
     get_footer();
