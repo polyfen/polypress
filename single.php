4d59a4d906f032122e41518e91d8c4153e6215bf
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 18:52:05 +0100

    Single post

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ Template Name: Blog Posts
     if ( have_posts() ) {
         while ( have_posts() ) {
             the_post();
-            the_content();
+            get_template_part( 'template-parts/content', 'blog' );
         }
     }
 ?>
