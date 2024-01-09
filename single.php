09e49b31e2f125e05614208d6c5ea8a006bc95a0
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 18:41:01 +0100

    Update single.php

---
+++ single.php
@@ -0,0 +1,20 @@
+<?php
+    get_header();
+?>
+
+<article class="container">
+
+<?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            the_content();
+        }
+    }
+?>
+
+</article>
+
+<?php
+    get_footer();
+?>
