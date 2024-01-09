55d2c42f02f7ce83789df831a002470962b562fe
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 20:39:34 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 ?>
 
 <section class="cover">
-    <h1 class="title-1 align-center">Upscale your Business<br>with our Proben Formula</h1>
+    <h1 class="title-1 align-center">Upscale your Business<br>with our Proven Formula</h1>
     <p class="align-center">We've helped Y Combinator start-ups<br>launch with amazing success.</p>
     <a class="button">Learn More</a>
 </section>
