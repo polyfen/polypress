1ac6f7d0c6d494fc833f715d381c588403e55bd8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-12 19:12:59 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -10,6 +10,20 @@
 
 <hr class="secondary-separator">
 
+<article>
+<?php
+
+    if( have_posts()){
+        while( have_posts()){
+            the_post();
+            the_content();
+        }
+    
+    }
+
+?>
+</article>
+
 <?php
     get_footer();
 ?>
